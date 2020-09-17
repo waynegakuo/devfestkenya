3e15a6037ad9f48b56292d62e4c6d985c217d349
waynegakuo <waynejotham@gmail.com>
2020-09-17 17:57:44 +0300

    Firebase environment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLNrGvp_pZQ4EsoArI4u41K9FuNkzZZs4',
+    authDomain: 'devfestkenya.firebaseapp.com',
+    databaseURL: 'https://devfestkenya.firebaseio.com',
+    projectId: 'devfestkenya',
+    storageBucket: 'devfestkenya.appspot.com',
+    messagingSenderId: '1090666581834',
+    appId: '1:1090666581834:web:22a1db3f0df062e2e5fbb3',
+    measurementId: 'G-X3286VPQ0Z'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLNrGvp_pZQ4EsoArI4u41K9FuNkzZZs4',
+    authDomain: 'devfestkenya.firebaseapp.com',
+    databaseURL: 'https://devfestkenya.firebaseio.com',
+    projectId: 'devfestkenya',
+    storageBucket: 'devfestkenya.appspot.com',
+    messagingSenderId: '1090666581834',
+    appId: '1:1090666581834:web:22a1db3f0df062e2e5fbb3',
+    measurementId: 'G-X3286VPQ0Z'
+  }
 };
 
 /*
